273e7f01498c87141e28870301e738bb73743adf
Glastaborg <jerryjohnabban@gmail.com>
2023-07-11 05:45:42 -0700

    Initial commit

---
+++ about.php
@@ -0,0 +1,299 @@
+<?php
+include("header.php");
+?>
+<div id="home-owl" class="owl-carousel owl-theme">
+
+<div class="home-item">
+
+<div class="section-bg" style="background-image: url(img/charity/Slides.jpg);"></div>
+
+<div class="home">
+<div class="container">
+<div class="row">
+<div class="col-md-8">
+<div class="home-content">
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="home-item">
+
+<div class="section-bg" style="background-image: url(img/charity/Slides02.jpg);"></div>
+
+
+<div class="home">
+<div class="container">
+<div class="row">
+<div class="col-md-8">
+<div class="home-content">
+
+</div>
+</div>
+</div>
+</div>
+</div>
+
+</div>
+
+
+<div class="home-item">
+
+<div class="section-bg" style="background-image: url(img/charity/Slides03.jpg);"></div>
+
+
+<div class="home">
+<div class="container">
+<div class="row">
+<div class="col-md-8">
+<div class="home-content">
+
+</div>
+</div>
+</div>
+</div>
+</div>
+
+</div>
+
+</div>
+
+</header>
+
+
+<div id="about" class="section">
+
+<div class="container">
+
+<div class="row">
+
+<div class="col-md-5">
+<div class="section-title">
+<h2 class="title"> Welcome to Help The Needy Foundation</h2>
+<p class="sub-title"> Help The Needy Foundation is a non-profit organization based in Kumasi, Ghana, dedicated to providing assistance to individuals and families who are facing financial challenges, health issues, or other forms of adversity.
+     The foundation's mission is to promote social justice and equity by empowering vulnerable populations with the resources and support they need to thrive. 
+     Their programs and services include food and clothing drives, medical assistance, educational support, and vocational training. With a team of dedicated volunteers and partners, Help The Needy Foundation strives to make a positive impact in the lives of those in need and create a brighter future for all. 
+     Help The Needy Foundation is proud to be a registered non-profit organization under the Ghana Non-profit Organization Registrar, reaffirming our commitment to transparency, accountability, and ethical conduct in serving the community.</p>
+</div>
+</div>
+
+
+<div class="col-md-offset-1 col-md-6">
+<a href="#" class="about-video">
+<img src="img/charity/About01.jpg" style="heighT: 500px;">
+</a>
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+
+<div id="numbers" class="section">
+
+<div class="container">
+
+<div class="row">
+
+<div class="col-md-3 col-sm-6">
+<div class="number">
+<i class="fa fa-smile-o"></i>
+<h3>1</h3>
+<span>Donors</span>
+</div>
+</div>
+
+
+<div class="col-md-3 col-sm-6">
+<div class="number">
+<i class="fa fa-heartbeat"></i>
+<h3>1.5K</h3>
+<span>Children Saved</span>
+</div>
+</div>
+
+
+<div class="col-md-3 col-sm-6">
+<div class="number">
+<i class="fa fa-money"></i>
+<h3>₵2.5K</h3>
+<span>Donated</span>
+</div>
+</div>
+
+
+<div class="col-md-3 col-sm-6">
+<div class="number">
+<i class="fa fa-handshake-o"></i>
+<h3>420</h3>
+<span>Volunteers</span>
+</div>
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+<hr>
+<div id="causes" class="section">
+
+<div class="container">
+
+<div class="row">
+
+<div class="col-md-12">
+<div class="section-title">
+<b>First Unit : Help The Needy Foundation</b>
+<p></p>
+<p class="sub-title"> Help The Needy Foundation is dedicated to addressing food insecurity in the rural area in Ashanti Region. This unit is focused on providing emergency food assistance to individuals and families who are struggling to make ends meet, as well as working to build sustainable solutions to combat hunger in the community. </p>
+<p class="sub-title"> The unit collaborates with local farmers and food producers to source nutritious, locally grown foods that are distributed through food banks, soup kitchens, and other channels. In addition to providing direct food assistance, the unit also works to raise awareness about food insecurity and its root causes, advocating for policies and programs that promote food justice and equity.</p>
+<p class="sub-title"> Through its work, the first unit of Help The Needy Foundation is making a meaningful difference in the lives of those who are most vulnerable and ensuring that everyone has access to the basic human right of food.....</p>
+<br>
+<b>Second Unit : Health Care Services</b>
+<p></p>
+<p class="sub-title"> The second unit of Help The Needy Foundation focuses on providing medical assistance to those in need in various rural areas in Kumasi and beyond. The unit is staffed by hired trained medical professionals who provide basic medical care, health education, and support to individuals and families who are struggling with health challenges.</p>
+<p class="sub-title"> The unit collaborates with local hospitals, clinics, and medical professionals to ensure that those in need receive the care they require. The unit also works to raise awareness about health issues and promote preventive care, particularly in areas where access to healthcare is limited.</p>
+<p class="sub-title"> Through its work, the second unit of Help The Needy Foundation is helping to improve health outcomes and reduce the burden of illness for vulnerable populations in the community...</p><br>
+
+
+<b>Third Unit : Education </b>
+<p></p>
+<p class="sub-title"> The third unit of Help The Needy Foundation is dedicated to providing educational support and opportunities to children and young adults in the Kumasi rural area and beyond. The unit works to ensure that every child has access to quality education, regardless of their background or economic status.</p>
+<p class="sub-title"> The unit provides scholarships, school supplies, and educational resources to students in need, as well as mentorship and tutoring services. Additionally, the unit works to improve school infrastructure and facilities, partnering with local schools to create safe, welcoming learning environments.</p>
+<p class="sub-title"> Through its work, the third unit of Help The Needy Foundation is helping to break down barriers to education and empower young people to reach their full potential.</p><br>
+
+
+<b>SERVICE.</b>
+<p></p>
+<p class="sub-title"> Help The Needy Foundation has a long-standing commitment to providing critical services to vulnerable populations in the Abuakwa-Tanoso area and beyond. Since its inception, the foundation has worked tirelessly to address pressing social issues, including food insecurity, health disparities, and educational barriers. Through its various units, the foundation provides a range of services, including emergency food assistance, basic medical care, health education, and educational support...</p>
+<p class="sub-title"> These services are delivered with compassion and respect, and are designed to empower individuals and families to overcome adversity and achieve their goals. The impact of the foundation's services is felt throughout the community, with countless individuals and families benefiting from their programs and initiatives. Whether it's a child who has been given the opportunity to attend school, a family who has access to nutritious food, or an individual who has received life-saving medical care, the foundation's services are making a meaningful difference in the lives of those in need.</p>
+<p class="sub-title"> Through their dedication and hard work, Help The Needy Foundation is helping to build a stronger, more resilient community for all. In addition to their direct services, Help The Needy Foundation also works to raise awareness about the root causes of social issues and advocate for systemic change. The foundation recognizes that while their programs and services are critical for addressing immediate needs, lasting change requires a broader, more holistic approach.</p>
+
+<p class="sub-title"> As such, they engage in community organizing, advocacy, and policy work to address the structural inequalities that contribute to poverty, food insecurity, and other social challenges. By working to create systemic change, Help The Needy Foundation is not only providing immediate relief to those in need, but also helping to create a more just and equitable society for all.
+</p>
+
+<p class="sub-title">We appeal to the privileged sections of the society
+                             to please extend their helping hand.
+</p>
+
+
+
+</div>
+</div>
+
+
+</div>
+
+</div>
+
+</div>
+
+
+<div id="cta" class="section">
+
+<div class="section-bg" style="background-image: url(img/background-5.jpg);" data-stellar-background-ratio="0.5"></div>
+
+
+<div class="container">
+
+<div class="row">
+
+<div class="col-md-offset-2 col-md-8">
+<div class="cta-content text-center">
+<h1>Join with Us</h1>
+<p class="lead">Your support is important to our work at The Help The Needy Foundation. There are many ways you can contribute towards our causes, and every little bit that you commit goes a long way in helping us fulfill our mission. Learn more about how you can get involved and take advantage of the opportunity to do some good...</p>
+</div>
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+
+<div id="events" class="section">
+
+<div class="container">
+
+<div class="row">
+
+<div class="col-md-8 col-md-offset-2">
+<div class="section-title text-center">
+<h2 class="title">Help The Needy Foundation </h2>
+</div>
+</div>
+
+
+<div class="col-md-6">
+<div class="event">
+<div class="event-img">
+<img src="img/event-01.jpg" alt="">
+</div>
+<div class="event-content">
+<h3>Help people in need</h3>
+<p>Provide direct support to an individual, family or community by paying medical expenses or offering financial aid..</p>
+</div>
+</div>
+</div>
+
+
+<div class="col-md-6">
+<div class="event">
+<div class="event-img">
+<img src="img/event-02.jpg" alt="">
+</div>
+<div class="event-content">
+<h3>Take action in an emergency</h3>
+
+<p>Raise funds in response to a natural disaster or humanitarian crisis. Make a difference in minutes.</p>
+</div>
+</div>
+</div>
+
+<div class="clearfix visible-md visible-lg"></div>
+
+<div class="col-md-6">
+<div class="event">
+<div class="event-img">
+<img src="img/event-03.jpg" alt="">
+</div>
+<div class="event-content">
+<h3>Take part in a charity event</h3>
+
+<p>Choose from hundreds of official events including marathons, bike rides, Dryathlons and bake offs….</p>
+</div>
+</div>
+</div>
+
+
+<div class="col-md-6">
+<div class="event">
+<div class="event-img">
+
+<img src="img/event-04.jpg" alt="">
+
+</div>
+<div class="event-content">
+<h3>Celebrate an occasion</h3>
+<p>Mark a special event like a birthday, wedding or final exam by asking friends for donations rather than gifts..</p>
+</div>
+</div>
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+
+<?php
+include("footer.php");
+?>
